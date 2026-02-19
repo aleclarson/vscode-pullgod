091c54c4b30f9c2d7c1a9bf0f2716bed4cd83c05
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2026-02-19 13:30:52 -0500

    wip

--- src/quickPick.ts
+++ src/quickPick.ts
@@ -9,9 +9,9 @@ export interface QuickPickItemProps {
 
 export function createQuickPickItem(pr: PullRequest): QuickPickItemProps {
   return {
-    label: `#${pr.number}`,
-    description: `${pr.title} by ${pr.author} (updated: ${new Date(pr.updatedAt).toLocaleString()})`,
-    detail: `${pr.headRefName} -> ${pr.baseRefName}`,
+    label: pr.title,
+    description: new Date(pr.updatedAt).toLocaleString(undefined, { hour: '2-digit', minute: '2-digit', year: 'numeric', month: '2-digit', day: '2-digit', second: undefined }),
+    detail: `(#${pr.number}) By ${pr.author} → "${pr.baseRefName}" branch`,
     pr: pr,
   };
 }
